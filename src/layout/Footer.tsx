ddd391cce93a7d25a90328b3d960ab59021c355f
wangbertha <wangbertha@gmail.com>
2025-02-07 13:41:56 -0600

    Create Navbar and Footer content

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,23 @@
 import "./../css/Footer.css";
 
 const Footer = () => {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <div>
+        Vectors and icons by{" "}
+        <a
+          href="https://vectordoodle.gumroad.com/l/FOCLd?ref=svgrepo.com"
+          target="_blank"
+        >
+          Vectordoodle
+        </a>{" "}
+        in CC Attribution License via{" "}
+        <a href="https://www.svgrepo.com/" target="_blank">
+          SVG Repo
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,7 +1,25 @@
+import { NavLink } from "react-router-dom";
 import "./../css/Navbar.css";
 
 const Navbar = () => {
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-site-info">
+        <img
+          width={96}
+          height={96}
+          src={new URL("./../assets/logo.svg", import.meta.url).href}
+          alt="Site logo"
+        />
+        Adopt-A-Dog
+      </div>
+      <menu>
+        <li>
+          <NavLink to="/login">Log Out</NavLink>
+        </li>
+      </menu>
+    </nav>
+  );
 };
 
 export default Navbar;

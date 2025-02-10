82aa422147fbd42a15eb3ec0efd3054d3b9d1966
wangbertha <wangbertha@gmail.com>
2025-02-10 01:35:56 -0600

    Add login and logout functionality

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,25 +1,45 @@
-import { NavLink } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./../css/Navbar.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const postLogout = async () => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BASE_URL + "/auth/logout",
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      );
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-site-info">
-        <NavLink to="/">
-          <img
-            width={96}
-            height={96}
-            src={new URL("./../assets/logo.svg", import.meta.url).href}
-            alt="Site logo"
-          />
-          Adopt-A-Dog
-        </NavLink>
+        <img
+          width={96}
+          height={96}
+          src={new URL("./../assets/logo.svg", import.meta.url).href}
+          alt="Site logo"
+        />
+        Adopt-A-Dog
       </div>
-      <menu>
-        <li>
-          <NavLink to="/login">Log Out</NavLink>
-        </li>
-      </menu>
+      {location.pathname.substring(0, 5) === "/dogs" && (
+        <menu>
+          <li onClick={postLogout}>Log Out</li>
+        </menu>
+      )}
     </nav>
   );
 };
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import "./../../css/Login.css";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [responseMessage, setResponseMessage] = useState<string>("");
 
   const navigate = useNavigate();
 
-  const postLogin = async (e): Promise<void> => {
+  const postLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    navigate("/dogs/search");
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BASE_URL + "/auth/login",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, email }),
+          credentials: "include",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      navigate("/dogs/search");
+    } catch (error) {
+      setResponseMessage(`Login was unsuccessful. ${error}`);
+    }
   };
 
   return (
@@ -22,7 +42,7 @@ const Login = () => {
           <p>Log in with your name and email below.</p>
           <p>No previous account is necessary.</p>
         </div>
-        <form onSubmit={postLogin}>
+        <form onSubmit={(e) => postLogin(e)}>
           <label>
             <div>Name</div>
             <input type="text" onChange={(e) => setName(e.target.value)} />
@@ -32,6 +52,7 @@ const Login = () => {
             <input type="email" onChange={(e) => setEmail(e.target.value)} />
           </label>
           <button type="submit">Log In</button>
+          <p>{responseMessage}</p>
         </form>
       </div>
     </div>
